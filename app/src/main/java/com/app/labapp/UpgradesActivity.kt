c113780e50020dc86a583ef1cebfe93abcb76cd5
anitakitsune <xplodjustboom@gmail.com>
2022-11-19 19:24:34 +0100

    side1

--- app/src/main/java/com/app/labapp/UpgradesActivity.kt
+++ app/src/main/java/com/app/labapp/UpgradesActivity.kt
@@ -1,15 +1,55 @@
 package com.app.labapp
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.google.android.material.snackbar.Snackbar
 
 class UpgradesActivity : AppCompatActivity() {
+
+    private val channelId = "10001"
+    private val defaultChannelId = "default"
+    private var notcontent:String = ""
+    private var notcontenttitle:String = ""
+
+    private fun createNotification(view: View) {
+        val notificationIntent = Intent(applicationContext, MainActivity::class.java)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(applicationContext, defaultChannelId)
+        notificationIntent.putExtra("fromNotification", true)
+        notificationIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        builder.setContentTitle(notcontenttitle)
+        builder.setContentIntent(pendingIntent)
+        builder.setContentText(notcontent)
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setAutoCancel(true)
+        builder.setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val notificationChannel = NotificationChannel(channelId, "NOTIFICATION_CHANNEL_NAME", importance)
+            builder.setChannelId(channelId)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        notificationManager.notify(System.currentTimeMillis().toInt(), builder.build())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upgrades)
@@ -46,8 +86,24 @@ class UpgradesActivity : AppCompatActivity() {
         var ach_up3_15 = prefs.getBoolean("ach_up3_15", false)
         var ach_up3_20 = prefs.getBoolean("ach_up3_20", false)
         var ach_all = prefs.getBoolean("ach_all", false)
-        val a1 = (R.string.not_enough)
-        val a2 = (R.string.upgraded)
+        val a1:String = resources.getString(R.string.not_enough)
+        val a2:String = resources.getString(R.string.upgraded)
+        val a3:String = resources.getString(R.string.ach_unl)
+        val a4:String = resources.getString(R.string.up_max)
+        val name1:String = resources.getString(R.string.ach_up1_5)
+        val name2:String = resources.getString(R.string.ach_up1_10)
+        val name3:String = resources.getString(R.string.ach_up1_15)
+        val name4:String = resources.getString(R.string.ach_up1_20)
+
+        val name5:String = resources.getString(R.string.ach_up2_5)
+        val name6:String = resources.getString(R.string.ach_up2_10)
+        val name7:String = resources.getString(R.string.ach_up2_15)
+        val name8:String = resources.getString(R.string.ach_up2_20)
+
+        val name9:String = resources.getString(R.string.ach_up3_5)
+        val name10:String = resources.getString(R.string.ach_up3_10)
+        val name11:String = resources.getString(R.string.ach_up3_15)
+        val name12:String = resources.getString(R.string.ach_up3_20)
 
         val upgrade1btn = findViewById<TextView>(R.id.up1)
         val upgrade2btn = findViewById<TextView>(R.id.up2)
@@ -65,11 +121,11 @@ class UpgradesActivity : AppCompatActivity() {
         str4.text = "$upgrade2cost"
         str5.text = "$upgrade3lvl"
         str6.text = "$upgrade3cost"
-        //cookies text
         text2.text = "$coins"
-        Handler(Looper.getMainLooper()).post(Runnable {
-            text2.text = coins.toString()
-        })
+
+        fun concat(s1: String, s2: String): String {
+            return "$s1 $s2"
+        }
 
         upgrade1btn.setOnClickListener{
             if (coins >= upgrade1cost){
@@ -81,37 +137,51 @@ class UpgradesActivity : AppCompatActivity() {
                 edit.putInt("coins", coins)
                 edit.putInt("upgrade1cost", upgrade1cost)
                 edit.putInt("upgrade1lvl", upgrade1lvl)
-                if (upgrade1lvl == 5){
-                    edit.putBoolean("ach_up1_5", true)
-                    val snack = Snackbar.make(it,"Achievement unlocked!", Snackbar.LENGTH_LONG)
-                    snack.show()
+                when (upgrade1lvl){
+                    5 -> {
+                        edit.putBoolean("ach_up1_5", true)
+                        val message = concat(a3, name1)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name1
+                        createNotification(upgrade1btn)
+                    }
+                    10 ->{
+                        edit.putBoolean("ach_up1_10", true)
+                        val message = concat(a3, name2)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name2
+                        createNotification(upgrade1btn)
+                    }
+                    15 ->{
+                        edit.putBoolean("ach_up1_15", true)
+                        val message = concat(a3, name3)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name3
+                        createNotification(upgrade1btn)
+                    }
+                    20 ->{
+                        edit.putBoolean("ach_up1_20", true)
+                        val message = concat(a3, name4)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name4
+                        createNotification(upgrade1btn)
+                    }
                 }
-                if (upgrade1lvl == 10){
-                    edit.putBoolean("ach_up1_10", true)
-                    val snack = Snackbar.make(it,"Achievement unlocked!", Snackbar.LENGTH_LONG)
-                    snack.show()
-                }
-                if (upgrade1lvl == 15){
-                    edit.putBoolean("ach_up1_10", true)
-                    val snack = Snackbar.make(it,"Achievement unlocked!", Snackbar.LENGTH_LONG)
-                    snack.show()
-                }
-                if (upgrade1lvl == 20){
-                    edit.putBoolean("ach_up1_10", true)
-                    val snack = Snackbar.make(it,"Achievement unlocked!", Snackbar.LENGTH_LONG)
-                    snack.show()
-                }
-                edit.apply()
+
                 text2.text = coins.toString()
                 str1.text = "$upgrade1lvl"
                 str2.text =  "$upgrade1cost"
                 val snack = Snackbar.make(it,a2, Snackbar.LENGTH_LONG)
                 snack.show()
-
             } else {
                 val snack = Snackbar.make(it,a1, Snackbar.LENGTH_LONG)
                 snack.show()
             }
+            edit.apply()
         }
         // upgrade2 button actions
         // add more cookies to combo
@@ -125,16 +195,50 @@ class UpgradesActivity : AppCompatActivity() {
                 edit.putInt("coins", coins)
                 edit.putInt("upgrade2cost", upgrade2cost)
                 edit.putInt("upgrade2lvl", upgrade2lvl)
-                edit.apply()
                 text2.text = coins.toString()
                 str3.text = "$upgrade2lvl"
                 str4.text = "$upgrade2cost"
+                when (upgrade2lvl){
+                    5 -> {
+                        val message = concat(a3, name5)
+                        edit.putBoolean("ach_up2_5", true)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name5
+                        createNotification(upgrade1btn)
+                    }
+                    10 ->{
+                        edit.putBoolean("ach_up2_10", true)
+                        val message = concat(a3, name6)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name6
+                        createNotification(upgrade1btn)
+                    }
+                    15 ->{
+                        edit.putBoolean("ach_up2_15", true)
+                        val message = concat(a3, name7)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name7
+                        createNotification(upgrade1btn)
+                    }
+                    20 ->{
+                        edit.putBoolean("ach_up2_20", true)
+                        val message = concat(a3, name8)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name8
+                        createNotification(upgrade1btn)
+                    }
+                }
                 val snack = Snackbar.make(it,a2, Snackbar.LENGTH_LONG)
                 snack.show()
             } else {
                 val snack = Snackbar.make(it,a1, Snackbar.LENGTH_LONG)
                 snack.show()
             }
+            edit.apply()
         }
         // upgrade3 button actions
         // lower combo tick rate
@@ -158,44 +262,73 @@ class UpgradesActivity : AppCompatActivity() {
                     str6.text = "$upgrade3cost"
 
                 }
+                when (upgrade3lvl){
+                    5 -> {
+                        edit.putBoolean("ach_up3_5", true)
+                        val message = concat(a3, name9)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name9
+                        createNotification(upgrade1btn)
+                    }
+                    10 ->{
+                        edit.putBoolean("ach_up3_10", true)
+                        val message = concat(a3, name10)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name10
+                        createNotification(upgrade1btn)
+                    }
+                    15 ->{
+                        edit.putBoolean("ach_up3_15", true)
+                        val message = concat(a3, name11)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name11
+                        createNotification(upgrade1btn)
+                    }
+                    20 ->{
+                        edit.putBoolean("ach_up3_20", true)
+                        val message = concat(a3, name12)
+//                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        notcontenttitle = a3
+                        notcontent = name12
+                        createNotification(upgrade1btn)
+                    }
+                }
                 if (tickrate in 1501..2000){
                     tickrate-=100
                     edit.putLong("tickrate", tickrate)
-                    edit.apply()
                     val snack = Snackbar.make(it,a2, Snackbar.LENGTH_LONG)
                     snack.show()
                 } else if (tickrate in 1001..1500){
                     tickrate-=80
                     edit.putLong("tickrate", tickrate)
-                    edit.apply()
                     val snack = Snackbar.make(it,a2, Snackbar.LENGTH_LONG)
                     snack.show()
                 } else if (tickrate in 501..1000){
                     tickrate-=50
                     edit.putLong("tickrate", tickrate)
-                    edit.apply()
                     val snack = Snackbar.make(it,a2, Snackbar.LENGTH_LONG)
                     snack.show()
                 } else if (tickrate in 101..500) {
                     tickrate-=20
                     edit.putLong("tickrate", tickrate)
-                    edit.apply()
                     val snack = Snackbar.make(it,a2, Snackbar.LENGTH_LONG)
                     snack.show()
                 } else if (tickrate in 10..100) {
                     tickrate-=10
                     edit.putLong("tickrate", tickrate)
-                    edit.apply()
                     val snack = Snackbar.make(it,a2, Snackbar.LENGTH_LONG)
                     snack.show()
                 } else {
                     edit.putLong("tickrate", 10)
-                    edit.apply()
                 }
             } else {
                 val snack = Snackbar.make(it,a1, Snackbar.LENGTH_LONG)
                 snack.show()
             }
+            edit.apply()
         }
     }
     override fun onSupportNavigateUp(): Boolean {
